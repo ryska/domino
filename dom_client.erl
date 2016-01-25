ea66410611c646b4d5724b0e1d59bd475fb79986
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2016-01-25 02:03:37 +0100

    Moar comments

--- dom_client.erl
+++ dom_client.erl
@@ -1,13 +1,32 @@
 -module(dom_client).
 -compile(export_all).
+%%%-----------------------------------------------------------------------------
+%%% Funkcje pomocnicze realizujace podstawowe interakcje klienta z serwerem
+%%%-----------------------------------------------------------------------------
 
+%%------------------------------------------------------------------------------
+%% Funkcja: register/5
+%% Cel: Rejestruje klienta na serwerze o podanym adresie i porcie. Klient jest
+%%     rejestrowany z podanym ID, nazwa i portem odbiorczym.
+%% Argumenty: Adres i port serwera, ID, nazwa i port odbiorczy klienta.
+%%------------------------------------------------------------------------------
 register(ServerAddress, ServerPort, Id, Name, ClientPort) ->
     {ok, Socket} = gen_udp:open(0, [binary, {active, false}]),
     gen_udp:send(Socket, ServerAddress, ServerPort, term_to_binary({register, Id, Name, ClientPort})).
 
+%%------------------------------------------------------------------------------
+%% Funkcja: data/4
+%% Cel: Wysyla dane na podany adres i port serwera od klienta o podanym ID.
+%% Argumenty: Adres i port serwera, ID i dane klienta.
+%%------------------------------------------------------------------------------
 data(ServerAddress, ServerPort, Id, Data) ->
     dom_net:send(ServerAddress, ServerPort, {data, Id, Data}).
 
+%%------------------------------------------------------------------------------
+%% Funkcja: delete/3
+%% Cel: Usuwa na serwerze o podanym adresie i porcie klienta o podanym ID.
+%% Argumenty: Adres i port serwera, ID klienta.
+%%------------------------------------------------------------------------------
 delete(ServerAddress, ServerPort, Id) ->
     {ok, Socket} = gen_udp:open(0, [binary, {active, false}]),
     gen_udp:send(Socket, ServerAddress, ServerPort, term_to_binary({delete, Id})).
--- dom_func.erl
+++ dom_func.erl
@@ -1,6 +1,14 @@
 -module(dom_func).
 -compile(export_all).
+%%%-----------------------------------------------------------------------------
+%%% Przykladowe funkcje reagujace dla zaimplementowanych czujnikow i odbiornikow.
+%%%-----------------------------------------------------------------------------
 
+%%------------------------------------------------------------------------------
+%% Funkcja: temp/1
+%% Cel: Reaguje na podana wartosc temperatury wlaczajac klimatyzacje w razie potrzeby.
+%% Argumenty: Wartosc temperatury.
+%%------------------------------------------------------------------------------
 temp(nil) -> nil;
 temp(Data) when Data > 28 ->
     io:format("Temperatura wynosi ~p, wlaczam klimatyzacje...~n", [Data]),
@@ -9,12 +17,22 @@ temp(_)  ->
     dom_server:send_to(klim, wylacz).
 
 
-
+%%------------------------------------------------------------------------------
+%% Funkcja: alarm/1
+%% Cel: Reaguje na stan alarmu, wysylajac SMS jesli zostal on aktywowany.
+%% Argumenty: Stan alarmu.
+%%------------------------------------------------------------------------------
 alarm(tak) ->
     io:format("Ktos wlamuje sie do domu, alarm!~n"),
     dom_server:send_to(sms, "Ktos wlamuje sie do domu, alarm!");
 alarm(_) -> nil.
 
+%%------------------------------------------------------------------------------
+%% Funkcja: alarm/1
+%% Cel: Reaguje na stan czujnika dymu, wysylajac SMS jesli zostal on aktywowany
+%%     oraz otwierajac okna.
+%% Argumenty: Stan czujnika dymu.
+%%------------------------------------------------------------------------------
 dym(tak) ->
     io:format("Czujnik wykryl dym!~n"),
     dom_server:send_to(sms, "Czujnik wykryl dym!"),
